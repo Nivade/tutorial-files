a4c44da5855f5eb7f86899aacb7074ead6fe51d8
Nivade <nhavandeursen@gmail.com>
2016-06-24 13:06:26 +0200

    Create puzzle component

--- angular2/puzzle/dev/app.component.ts
+++ angular2/puzzle/dev/app.component.ts
@@ -3,8 +3,6 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
     `,
 })
 export class AppComponent {
