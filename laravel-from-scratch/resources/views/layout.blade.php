fec7b2fa75d258f37e51c7ed49f110427f596e7e
Nivade <nhavandeursen@gmail.com>
2016-06-25 17:30:21 +0200

    Squashed 'laracasts/' content from commit 6297b30
    git-subtree-dir: laracasts
    git-subtree-split: 6297b30fb0bced37f78b11676892d2e0f09231c3

---
+++ laravel-from-scratch/resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+        <link rel="stylesheet" type="text/css" href="{{ elixir('css/app.css') }}">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+
+        @yield('content')
+
+    </body>
+</html>
