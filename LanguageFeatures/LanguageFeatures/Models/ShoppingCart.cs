12c1a803ad77c7ccbf31d115de7a66a4d141003a
Nivade <nhavandeursen@gmail.com>
2016-08-04 16:55:43 +0200

    implement IEnumerable

--- LanguageFeatures/LanguageFeatures/Models/ShoppingCart.cs
+++ LanguageFeatures/LanguageFeatures/Models/ShoppingCart.cs
@@ -1,13 +1,33 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace LanguageFeatures.Models
 {
-    public class ShoppingCart
+    public class ShoppingCart : IEnumerable<Product>
     {
 
         public List<Product> Products { get; set; }
+
+
+
+        /// <summary>Returns an enumerator that iterates through the collection.</summary>
+        /// <returns>A <see cref="T:System.Collections.Generic.IEnumerator`1" /> that can be used to iterate through the collection.</returns>
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+
+
+
+        /// <summary>Returns an enumerator that iterates through a collection.</summary>
+        /// <returns>An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
     }
 }
