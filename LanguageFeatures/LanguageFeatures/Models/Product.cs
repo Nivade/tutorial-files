06ae42805e070ddf8fb6f10fd4e939b2cbc559ff
Nivade <nhavandeursen@gmail.com>
2016-08-04 16:25:31 +0200

    add auto properties

--- LanguageFeatures/LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -7,15 +7,18 @@ namespace LanguageFeatures.Models
 {
     public class Product
     {
+        public int ProductID { get; set; }
 
-        private string name;
+
+        public string Name { get; set; }
 
 
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
+        public string Description { get; set; }
 
+
+        public decimal Price { get; set; }
+
+
+        public string Category { get; set; }
     }
 }
