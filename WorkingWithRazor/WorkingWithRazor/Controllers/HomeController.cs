65aa33bf611286b8319b30ed34dc6aa5f548e2bf
Nivade <nhavandeursen@gmail.com>
2016-08-07 14:30:27 +0200

    add home controller and index view

---
+++ WorkingWithRazor/WorkingWithRazor/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WorkingWithRazor.Models;
+
+
+namespace WorkingWithRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private Product myProduct = new Product
+        {
+            ProductID = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275m
+        };
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+    }
+}
