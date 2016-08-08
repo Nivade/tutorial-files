87271abd02095e188b82ff6a15c195e34c9a0193
Nivade <nhavandeursen@gmail.com>
2016-08-08 12:32:45 +0200

    add ninject dependency resolver

--- EssentialTools/EssentialTools/App_Start/NinjectWebCommon.cs
+++ EssentialTools/EssentialTools/App_Start/NinjectWebCommon.cs
@@ -1,3 +1,5 @@
+using EssentialTools.Infrastructure;
+
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(EssentialTools.App_Start.NinjectWebCommon), "Start")]
 [assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(EssentialTools.App_Start.NinjectWebCommon), "Stop")]
 
@@ -61,6 +63,7 @@ namespace EssentialTools.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            System.Web.Mvc.DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
         }        
     }
 }
