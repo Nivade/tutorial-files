383a8192178600f73ed1c18c19e004977138a505
Niels van Deursen <nhavandeursen@gmail.com>
2016-08-22 20:59:31 +0200

    Add checkout controller action and view

--- SportsStore/SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/CartController.cs
@@ -63,5 +63,10 @@ namespace SportsStore.WebUI.Controllers
             return PartialView(cart);
         }
 
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
+
     }
 }
