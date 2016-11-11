5f2cb8de980a19db0970a01fcaeeaa00f19863a8
Niels van Deursen <nhavandeursen@gmail.com>
2016-11-11 03:25:13 +0100

    implement my upcoming gigs

---
+++ GigHub/GigHub/Controllers/FollowingsController.cs
@@ -0,0 +1,43 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    public class FollowingDto
+    {
+        public string FolloweeId { get; set; }
+    }
+
+    [Authorize]
+    public class FollowingsController : ApiController
+    {
+        private ApplicationDbContext context;
+
+        public FollowingsController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (context.Followings.Any(f => f.FolloweeId == userId && f.FolloweeId == dto.FolloweeId))
+                return BadRequest("Following already exists.");
+
+            var following = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = dto.FolloweeId
+            };
+
+            context.Followings.Add(following);
+            context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -1,6 +1,8 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
 using Microsoft.AspNet.Identity;
+using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -14,6 +16,18 @@ namespace GigHub.Controllers
         public GigsController()
         {
             context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = context.Gigs
+                .Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now)
+                .Include(g => g.Genre)
+                .ToList();
+
+            return View(gigs);
         }
 
         [Authorize]
@@ -51,7 +65,28 @@ namespace GigHub.Controllers
             context.Gigs.Add(gig);
             context.SaveChanges();
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Gigs");
+        }
+
+        [Authorize]
+        public ActionResult Attending()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = context.Attendances
+                .Where(a => a.AttendeeId == userId)
+                .Select(a => a.Gig)
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
+                .ToList();
+
+            var viewModel = new GigsViewModel
+            {
+                UpcomingGigs = gigs,
+                ShowActions = User.Identity.IsAuthenticated,
+                Heading = "Attending"
+            };
+
+            return View("Gigs", viewModel);
         }
     }
 }
